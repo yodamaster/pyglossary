5c56a52109b3d93f70d3f1768cc06ccc5f11e799
Saeed Rasooli <saeed.gnu@gmail.com>
2016-07-14 04:05:14 +0430

    appledict drafts

--- pyglossary/plugins/appledict/__init__.py
+++ pyglossary/plugins/appledict/__init__.py
@@ -25,7 +25,9 @@ import pkgutil
 import shutil
 
 from pyglossary.plugins.formats_common import *
-from ._dict import write_xml, get_beautiful_soup
+from ._dict import *
+
+import xdxf
 
 sys.setrecursionlimit(10000)
 
@@ -41,23 +43,26 @@ writeOptions = [
     'defaultPrefs',
     'prefsHTML',
     'frontBackMatter',
-    'OtherResources',
     'jing',
     'indexes',
 ]
-
-OtherResources = 'OtherResources'
 
 
 def abspath_or_None(path):
     return os.path.abspath(os.path.expanduser(path)) if path else None
 
 
-def write_xsl(xsl):
-    if not xsl:
-        return
-    with indir(OtherResources, create=True):
-        shutil.copyfile(xsl, os.path.basename(xsl))
+def write_header(glos, toFile, frontBackMatter):
+    # write header
+    toFile.write(
+        '<?xml version="1.0" encoding="UTF-8"?>\n'
+        '<d:dictionary xmlns="http://www.w3.org/1999/xhtml" '
+        'xmlns:d="http://www.apple.com/DTDs/DictionaryService-1.0.rng">\n'
+    )
+
+    if frontBackMatter:
+        with open(frontBackMatter, 'r') as front_back_matter:
+            toFile.write(front_back_matter.read())
 
 
 def format_default_prefs(defaultPrefs):
@@ -87,10 +92,9 @@ def write_plist(glos, filename, xsl, defaultPrefs, prefsHTML, frontBackMatter):
         'project_templates/Info.plist',
     ))
 
-    basename = os.path.splitext(filename)[0]
     # identifier must be unique
     # use file base name
-    identifier = basename.replace(' ', '')
+    identifier = filename.replace(' ', '')
 
     if bs4:
         # strip html tags
@@ -103,11 +107,11 @@ def write_plist(glos, filename, xsl, defaultPrefs, prefsHTML, frontBackMatter):
 
     # if DCSDictionaryXSL provided but DCSDictionaryDefaultPrefs <dict/> not
     # present in Info.plist, Dictionary.app will crash.
-    with open(filename, 'w') as f:
-        f.write(template % {
+    with open(filename, 'w') as toFile:
+        toFile.write(template % {
             "CFBundleIdentifier": identifier,
             "CFBundleDisplayName": glos.getInfo('name'),
-            "CFBundleName": basename,
+            "CFBundleName": filename,
             "DCSDictionaryCopyright": copyright,
             "DCSDictionaryManufacturerName": glos.getInfo('author'),
             "DCSDictionaryXSL": (os.path.basename(xsl) if xsl else ""),
@@ -122,68 +126,26 @@ def write_plist(glos, filename, xsl, defaultPrefs, prefsHTML, frontBackMatter):
 
 
 def write_css(fname, css_file):
-    if css_file:
-        with open(css_file, 'rb') as f:
-            css = f.read()
-    else:
-        css = pkgutil.get_data(__name__, 'project_templates/Dictionary.css')
-    with open(fname, 'wb') as f:
-        f.write(css)
-
-
-def write_makefile(dict_name):
-    template = toStr(pkgutil.get_data(__name__, 'project_templates/Makefile'))
-    with open('Makefile', 'w') as f:
-        f.write(template % {'dict_name': dict_name})
-
-
-def write_prefsHTML(prefsHTML_file):
-    if not prefsHTML_file:
-        return
-    with indir(OtherResources, create=True):
-        shutil.copyfile(prefsHTML_file, os.path.basename(prefsHTML_file))
-
-
-def write_resources(paths):
-    """
-    copy files and directories 'paths' to 'OtherResources'.
-    each item of 'paths' must be an absolute path.
-    """
-    if not paths:
-        return
-    with indir(OtherResources, create=True):
-        # cannot just shutil.copytree as it will fail with error if
-        # destination exists, but we want to merge instead.
-        for path in paths:
-            name = os.path.split(path)[1]
-            if os.path.isdir(path):
-                shutil.copytree(path, name)
-            else:
-                shutil.copy2(path, name)
-
-
-def safe_listdir_set(path):
-    """
-    :rtype: set
-    """
-    if not path:
-        return set()
-    if not os.path.isdir(path):
-        log.error("resource path is not a directory: %r" % path)
-        return set()
-    return {os.path.join(path, node) for node in os.listdir(path)}
+    with open(fname, 'wb') as toFile:
+        if css_file:
+            with open(css_file, 'rb') as fromFile:
+                toFile.write(fromFile.read())
+        else:
+            toFile.write(pkgutil.get_data(
+                __name__,
+                'project_templates/Dictionary.css',
+            ))
 
 
 def write(
     glos,
-    fpath,
-    cleanHTML="yes",
+    dirPath,
+    cleanHTML=True,
     css=None,
     xsl=None,
     defaultPrefs=None,
     prefsHTML=None,
     frontBackMatter=None,
-    OtherResources=None,
     jing=None,
     indexes=None,
 ):
@@ -191,7 +153,7 @@ def write(
     write glossary to Apple dictionary .xml and supporting files.
 
     :type glos: pyglossary.glossary.Glossary
-    :type fpath: str
+    :type dirPath: str, directory path, must not have extension
 
     :type cleanHTML: str
     :param cleanHTML: pass "yes" to use BeautifulSoup parser.
@@ -219,10 +181,6 @@ def write(
         your front/back matter entry content
     </d:entry>
 
-    :type OtherResources: str or None
-    :param OtherResources: path to 'OtherResources' directory.  Apple
-    recommending store images in 'OtherResources/Images'.
-
     :type jing: str or None
     :param jing: pass "yes" to run Jing check on generated XML.
 
@@ -232,69 +190,94 @@ def write(
     additional indexes to dictionary entries.
     # for now no languages supported yet.
     """
-    basename = os.path.splitext(fpath)[0]
-    dict_name = os.path.split(basename)[1]
+    xdxf.xdxf_init()
+
+    if cleanHTML:
+        BeautifulSoup = get_beautiful_soup()
+        if not BeautifulSoup:
+            log.warning(
+                'cleanHTML option passed but BeautifulSoup not found.  '
+                'to fix this run `easy_install beautifulsoup4` or '
+                '`pip3 install beautifulsoup4`.'
+            )
+    else:
+        BeautifulSoup = None
+
+    fileNameBase = split(dirPath)[1].replace('.', '_')
+    filePathBase = join(dirPath, fileNameBase)
     # before chdir (outside indir block)
     css = abspath_or_None(css)
     xsl = abspath_or_None(xsl)
     prefsHTML = abspath_or_None(prefsHTML)
     frontBackMatter = abspath_or_None(frontBackMatter)
     glos.resPath = abspath_or_None(glos.resPath)
-    OtherResources = abspath_or_None(OtherResources)
 
-    # to avoid copying css, prefs or something like that.
-    res = safe_listdir_set(glos.resPath).union(
-        safe_listdir_set(OtherResources)
+    generate_id = id_generator()
+    generate_indexes = indexes_generator(indexes)
+
+    glos.setDefaultDefiFormat('h')
+
+    with open(filePathBase + '.xml') as toFile:
+        write_header(glos, toFile, frontBackMatter)
+        for entryI, entry in enumerate(glos):
+            if glos.isData():
+                entry.save(OtherResources)
+                continue
+
+            words = entry.getWords()
+            word, alts = words[0], words[1:]
+            defi = entry.getDefi()
+
+            long_title = _normalize.title_long(
+                _normalize.title(word, BeautifulSoup)
+            )
+            if not long_title:
+                continue
+
+            _id = next(generate_id)
+            if BeautifulSoup:
+                title_attr = BeautifulSoup.dammit.EntitySubstitution\
+                    .substitute_xml(long_title, True)
+            else:
+                title_attr = '"%s"' % long_title
+
+            content_title = long_title
+            if entry.getDefiFormat() == 'x':
+                defi = xdxf.xdxf_to_html(defi)
+                content_title = None
+            content = format_clean_content(content_title, defi, BeautifulSoup)
+
+            toFile.write(
+                '<d:entry id="%s" d:title=%s>\n' % (_id, title_attr) +
+                generate_indexes(long_title, alts, content, BeautifulSoup) +
+                content +
+                '\n</d:entry>\n'
+            )
+
+        toFile.write('</d:dictionary>\n')
+
+    if xsl:
+        shutil.copy(xsl, OtherResources)
+    if prefsHTML_file:
+        shutil.copy(prefsHTML_file, OtherResources)
+    write_plist(
+        glos,
+        filePathBase + '.plist',
+        xsl=xsl,
+        defaultPrefs=defaultPrefs,
+        prefsHTML=prefsHTML,
+        frontBackMatter=frontBackMatter,
     )
-    res -= {css, xsl, prefsHTML, frontBackMatter}
+    write_css(filePathBase + '.css', css)
 
-    with indir(basename, create=True, clear=True):
-        write_plist(
-            glos,
-            dict_name + '.plist',
-            xsl=xsl,
-            defaultPrefs=defaultPrefs,
-            prefsHTML=prefsHTML,
-            frontBackMatter=frontBackMatter,
+    with open(join(dirPath, 'Makefile'), 'w') as toFile:
+        toFile.write(
+            toStr(pkgutil.get_data(
+                __name__,
+                'project_templates/Makefile',
+            )) % {'dict_name': dict_name}
         )
-        write_xml(
-            glos,
-            dict_name + '.xml',
-            cleanHTML == "yes",
-            frontBackMatter=frontBackMatter,
-            indexes=indexes,
-        )
-        write_css(dict_name + '.css', css)
-        write_makefile(dict_name)
-        write_xsl(xsl)
-        write_prefsHTML(prefsHTML)
-        write_resources(res)
-        if jing == "yes":
-            from .jing import run
-            run(dict_name + '.xml')
 
-if __name__ == '__main__':
-    import sys
-    import os.path
-    import glossary as gl
-    glos = gl.Glossary()
-
-    informat = gl.Glossary.descFormat["Babylon (bgl)"]
-    outformat = gl.Glossary.descFormat["AppleDict Source (xml)"]
-
-    filename = sys.argv[1]
-    basename = os.path.splitext(os.path.basename(filename))[0]
-
-    outdir = sys.argv[2]
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-
-    glos.convert(
-        filename,
-        inputFormat=informat,
-        readOptions=dict(
-            resPath=os.path.join(outdir, "OtherResources"),
-        ),
-        outputFilename=os.path.join(outdir, basename),
-        outputFormat=outformat,
-    )
+    if jing == "yes":
+        from .jing import run as jing_run
+        jing_run(filePathBase + '.xml')
