40e4abce7f4b33e9f9567b286045bed7c99a7d13
Saeed Rasooli <saeed.gnu@gmail.com>
2016-07-15 03:25:35 +0430

    BGL reader: improvement: show "unknown html entity" debugs at last only

--- pyglossary/plugins/babylon_bgl/bgl_reader.py
+++ pyglossary/plugins/babylon_bgl/bgl_reader.py
@@ -59,6 +59,7 @@ from .bgl_text import (
     replaceAsciiCharRefs,
     fixImgLinks,
     stripDollarIndexes,
+    unkownHtmlEntries,
 )
 
 file = io.BufferedReader
@@ -504,6 +505,10 @@ class BglReader(object):
 
     def __del__(self):
         self.close()
+        while unkownHtmlEntries:
+            log.debug(
+                'BGL: unknown html entity: %s' % unkownHtmlEntries.pop()
+            )
 
     # returns False if error
     def readBlock(self, block):
